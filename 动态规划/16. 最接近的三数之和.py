5b5deef6233365d55f2a02337c73f7a1e2c62563
no-email <987233735@qq.com>
2023-03-02 11:15:07 +0800

    动态规划

--- 动态规划/16. 最接近的三数之和.py
+++ 动态规划/16. 最接近的三数之和.py
@@ -14,3 +14,45 @@
 输出：0
 """
 
+def colsesum(nums,target):
+    nums.sort()
+    n = len(nums)
+    if n ==3:
+        return sum(nums)
+    colse = 10e7
+
+    for i in range(n):
+        z,j = i+1,n-1
+        while z<j:
+            temp = nums[i] + nums[j] + nums[z]
+            if temp == target:
+                return temp
+            if abs(temp - target)<abs(colse-target):
+                colse = temp
+            if temp>target:
+                j = j-1
+            if temp<target:
+                z = z+1
+    return colse
+
+    # while i<j:
+    #     for z in range(i+1,j):
+    #         temp = nums[i]+nums[j]+nums[z] - target
+    #         if abs(colse)>=abs(temp):
+    #             colse = temp
+    #     if colse==0:
+    #         return target
+    #     elif colse<0:
+    #         i = i + 1
+    #     else:
+    #         j = j - 1
+    # return colse+target
+print(colsesum([4,0,5,-5,3,3,0,-4,-5],-2))
+print(colsesum([-1,2,1,-4],1))
+
+
+
+
+
+
+
